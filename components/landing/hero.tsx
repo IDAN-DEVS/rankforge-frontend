a5537e187792443cbedc613c5b8584ceb9155dc4
solomonadzape <adzapesolomon@gmail.com>
2025-06-02 11:52:10 +0100

    Refactor landing components for improved structure and styling
    - Updated FAQSection to enhance layout and interactivity with new state management.
    - Simplified Features component by removing unused animations and integrating Heroicons for better visual representation.
    - Refined HowItWorks component to clarify steps and improve user engagement with new card layouts.
    - Adjusted Hero component for better spacing and added rounded styles to images.
    - Modified global styles to exclude pointer events for specific images, enhancing user experience.

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -5,14 +5,13 @@ import { Button } from "../ui/button";
 
 const Hero: React.FC = () => {
   return (
-    <div className="mt-40 w-full min-h-screen flex flex-col items-center justify-center gap-16 inter mb-20">
-      <section className=" max-w-xl flex-1 flex flex-col gap-7 items-center text-center">
+    <div className="pt-52 w-full min-h-screen max-h-[130vh] flex flex-col items-center justify-center gap-16 inter mb-30 overflow-hidden relative">
+      <section className="mt-40 max-w-xl flex-1 flex flex-col gap-7 items-center text-center">
         <h1 className=" text-6xl font-bold text-[#ebebeb]">
           Shine the spotlight on{" "}
           <span className="relative inline-block">
             <span className="relative z-10 text-[#DAFF01]">your work</span>
             <img
-              
               src="/images/swipe.png"
               alt="underline"
               className="absolute -left-2 right-0 -bottom-4 w-full h-[0.5em] z-0"
@@ -30,7 +29,7 @@ const Hero: React.FC = () => {
         <img
           src="/images/dashboard.png"
           alt="hero"
-          className="w-7xl h-full hover:scale-[101%] transition-all duration-300 ease-in-out hover:drop-shadow-[0_8px_20px_rgba(45,32,51,0.7)] hover:cursor-pointer"
+          className="w-7xl h-full hover:scale-[101%] transition-all duration-300 ease-in-out hover:drop-shadow-[0_8px_20px_rgba(45,32,51,0.7)] hover:cursor-pointer rounded-xl dashboard-img"
         />
       </section>
     </div>
